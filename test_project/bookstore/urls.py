68df3386970ff2427b67c45c3584da4b2c488246
vmichailcc <vmichail2022@gmail.com>
2022-06-13 14:53:01 +0300

    HW w2 add url

--- test_project/bookstore/urls.py
+++ test_project/bookstore/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('firstapp/', include('firstapp.urls')),
-    path('', include('bookstore.urls')),
+    path('', index, name="index"),
 
 ]
