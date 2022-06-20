79771e31e321b275debb01a8c9fce17a1cec4990
vmichailcc <vmichail2022@gmail.com>
2022-06-20 14:22:51 +0300

    HW w3 add models

--- test_project/bookstore/models.py
+++ test_project/bookstore/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=250, verbose_name="Title")
+    released_year = models.IntegerField(max_length=4, verbose_name="Released year")
+    description = models.TextField(max_length=5000, verbose_name="Description")
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    author_id = models.ForeignKey("Author", on_delete=models.CASCADE, verbose_name="Author")
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.IntegerField(max_length=3)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
